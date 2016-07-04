a8b1b70dda9e79e8b41ff57387659f9116fba326
Sergii Ivanov sergii.ivanov@teamdev.com <sergii.ivanov@teamdev.com>
2016-07-04 16:22:36 +0300

    initial RESTful Heroes service

---
+++ WebApplication1/Controllers/HeroesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HeroesController : ApiController
+    {
+        // GET: api/Heroes
+        public IList<Hero> Get()
+        {
+            return HeroesStorage.getInstance().getAll();
+        }
+
+        // GET: api/Heroes/{id}
+        public string Get(int id)
+        {
+            return HeroesStorage.getInstance().getHero(id);
+        }
+
+        // POST: api/Heroes/
+        public void Post([FromBody] string name)
+        {
+            HeroesStorage.getInstance().updateHero(-1, name);
+        }
+
+        // PUT: api/Heroes/
+        public void Put([FromBody]int id, [FromBody]string name)
+        {
+            HeroesStorage.getInstance().updateHero(id, name);
+        }
+
+        // DELETE: api/Heroes/
+        public void Delete([FromBody] int id)
+        {
+            HeroesStorage.getInstance().removeHero(id);
+        }
+    }
+}
---
+++ WebApplication1/Models/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Hero
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public static Hero build(int _id, string _name)
+        {
+            Hero h = new Hero();
+            h.id = _id;
+            h.name = _name;
+            return h;
+        }
+    }
+
+}
